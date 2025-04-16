9721207177c7c0abd325c927de59497937958968
Matheus Ventura de Oliveira <matheus.ventura.oliveira@escola.pr.gov.br>
2025-04-16 09:26:11 -0300

    Delete Sistema de login directory

--- Sistema de login/login.php
+++
@@ -1,9 +0,0 @@
-<?phpinclude 'db.pdp';
-
-if ($_server["REQUEST_METHOD"] == "POST") {
-    $username = $_POST{"username"};
-    $password = $_POST{"password"};
-
-    $sql = "SELECT * FROM users WHERE username = ? AND password = ?";
-    $stmt = $conn->prepare($sql);
-}
