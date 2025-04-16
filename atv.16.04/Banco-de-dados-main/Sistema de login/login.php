7bdcf2b47dbf2365baf0f08be5794bf455418b4f
Matheus Ventura de Oliveira <matheus.ventura.oliveira@escola.pr.gov.br>
2025-04-16 19:24:51 -0300

    Update login.php

--- atv.16.04/Banco-de-dados-main/Sistema de login/login.php
+++ atv.16.04/Banco-de-dados-main/Sistema de login/login.php
@@ -6,4 +6,19 @@ if ($_server["REQUEST_METHOD"] == "POST") {
 
     $sql = "SELECT * FROM users WHERE username = ? AND password = ?";
     $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $username, $passaword);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        $_SESSION["loggedin"] = true;
+        $_SESSION["username"] = $username;
+        header("Location: home.php");
+        exit();
+    } else }
+        echo "Usuário ou senha incorretos.";
+    }
+    $stmt->close();
+$conn->close();
 }
+?>
